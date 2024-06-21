a4975468d1a1fd60426fe68823138dd8e1fa2730
Jeferson <jbasurco@unsa.edu.pe>
2024-06-21 12:53:47 -0500

    Definiendo palabras para adivinar

--- juego_ahorcado/src/app/servicios/palabras.service.ts
+++ juego_ahorcado/src/app/servicios/palabras.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class PalabrasService {
+export class PalabrasService {  
 
+  private palabras: string[] = [
+    'alexandra',
+    'paul',
+    'joao',
+    'esternocleidomastoideo',
+  ]
   constructor() { }
+  
+  getPalabras(): string[] {
+    return this.palabras
+  }
 }
