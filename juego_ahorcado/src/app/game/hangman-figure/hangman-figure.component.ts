d4b5aca35315f11624b0648188ca898c43f2803f
aquispearr <aquispearr@unsa.edu.pe>
2024-06-22 17:44:37 -0500

    Importa y utiliza el servicio Palabras en el componente hangman

--- juego_ahorcado/src/app/game/hangman-figure/hangman-figure.component.ts
+++ juego_ahorcado/src/app/game/hangman-figure/hangman-figure.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PalabrasService } from '../servicios/palabras.service';
 
 @Component({
   selector: 'app-hangman-figure',
   standalone: true,
   imports: [],
   templateUrl: './hangman-figure.component.html',
-  styleUrl: './hangman-figure.component.css'
+  styleUrls: ['./hangman-figure.component.css']
 })
-export class HangmanFigureComponent {
+export class HangmanFigureComponent implements OnInit {
+  partesCuerpo = ['head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
 
+  constructor(public palabrasService: PalabrasService) {}
+
+  ngOnInit() {
+    this.palabrasService.intentosCambio.subscribe((intentosRestantes) => {
+      this.actualizarCuerpo(intentosRestantes);
+    });
+  }
+
+  actualizarCuerpo(intentosRestantes: number) {
+    const partesAVisible = this.partesCuerpo.slice(0, 6 - intentosRestantes);
+    this.partesCuerpo.forEach(parte => {
+      const elemento = document.querySelector(`.body-parts .${parte}`);
+      if (elemento) {
+        if (partesAVisible.includes(parte)) {
+          elemento.classList.remove('hidden');
+          elemento.classList.add('visible');
+          (elemento as HTMLElement).style.backgroundColor = '#ffffff';
+        } else {
+          elemento.classList.remove('visible');
+          elemento.classList.add('hidden');
+          (elemento as HTMLElement).style.backgroundColor = '#ffffff00';
+        }
+      }
+    });
+  }
 }
