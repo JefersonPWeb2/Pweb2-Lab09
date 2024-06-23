6276d6e4b40f7b0e995dec2f053d2b7226b854d8
Jeferson <jbasurco@unsa.edu.pe>
2024-06-22 20:04:22 -0500

    Creación de un alfabeto, método adivinar, mensaje final y reinicio del juego

--- juego_ahorcado/src/app/game/game-board/game-board.component.ts
+++ juego_ahorcado/src/app/game/game-board/game-board.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PalabrasService } from '../servicios/palabras.service';
 
 @Component({
   selector: 'app-game-board',
-  standalone: true,
-  imports: [],
   templateUrl: './game-board.component.html',
-  styleUrl: './game-board.component.css'
+  styleUrls: ['./game-board.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class GameBoardComponent {
+  alphabet: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  letrasAdivinadas: Set<string> = new Set();
 
+  constructor(public palabrasService: PalabrasService) {}
+
+  adivinar(letter: string) {
+    this.letrasAdivinadas.add(letter);
+    this.palabrasService.adivinar(letter.toLowerCase());
+  }
+
+  getEndMessage(): string {
+    if (this.palabrasService.intentos <= 0) {
+      return '¡Perdiste! La palabra era: ' + this.palabrasService.palabraActual;
+    } else {
+      return '¡Felicidades! Ganaste';
+    }
+  }
+
+  reiniciarJuego() {
+    this.letrasAdivinadas.clear();
+    this.palabrasService.nuevaPalabra();
+  }
 }
