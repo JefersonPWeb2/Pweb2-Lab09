84cf07d0bc26c089acf9e36ca581aec3edffde52
Jeferson <jbasurco@unsa.edu.pe>
2024-06-22 20:20:00 -0500

    Creación de constructor y metodo getImagenURL para retornar una ruta

--- juego_ahorcado/src/app/game/hangman-figure/hangman-figure.component.ts
+++ juego_ahorcado/src/app/game/hangman-figure/hangman-figure.component.ts
@@ -1,39 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PalabrasService } from '../servicios/palabras.service';
 
 @Component({
   selector: 'app-hangman-figure',
-  standalone: true,
-  imports: [],
   templateUrl: './hangman-figure.component.html',
-  styleUrls: ['./hangman-figure.component.css']
+  styleUrls: ['./hangman-figure.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
-export class HangmanFigureComponent implements OnInit {
-  partesCuerpo = ['head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
+export class HangmanFigureComponent {
+  @Input() intentos: number = 0;
 
   constructor(public palabrasService: PalabrasService) {}
 
-  ngOnInit() {
-    this.palabrasService.intentosCambio.subscribe((intentosRestantes) => {
-      this.actualizarCuerpo(intentosRestantes);
-    });
-  }
-
-  actualizarCuerpo(intentosRestantes: number) {
-    const partesAVisible = this.partesCuerpo.slice(0, 6 - intentosRestantes);
-    this.partesCuerpo.forEach(parte => {
-      const elemento = document.querySelector(`.body-parts .${parte}`);
-      if (elemento) {
-        if (partesAVisible.includes(parte)) {
-          elemento.classList.remove('hidden');
-          elemento.classList.add('visible');
-          (elemento as HTMLElement).style.backgroundColor = '#ffffff';
-        } else {
-          elemento.classList.remove('visible');
-          elemento.classList.add('hidden');
-          (elemento as HTMLElement).style.backgroundColor = '#ffffff00';
-        }
-      }
-    });
+  getImageUrl(): string {
+    const imageNumber = 9 - this.palabrasService.intentos;
+    return `assets/hangman/${imageNumber}.png`;
   }
 }
