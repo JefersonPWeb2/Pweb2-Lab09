6788bc3656ed60899bf42f20fe9352868e382f69
Jeferson <jbasurco@unsa.edu.pe>
2024-06-23 20:04:55 -0500

    Arreglando ultimos detalles

--- juego_ahorcado/src/app/game/servicios/palabras.service.ts
+++ juego_ahorcado/src/app/game/servicios/palabras.service.ts
@@ -21,7 +21,6 @@ export class PalabrasService {
     'contrarrevolucionario',
     'interdisciplinariedad',
     'caleidoscopio',
-    'supercalifragilisticoespialidoso',
     'desproporcionadamente',
     'incomprehensibilidad'
   ];
